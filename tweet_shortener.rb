dd29d95ed5e50051bca6972678fa3e327778ee6f
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-30 18:32:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,38 @@
 # Write your code here.
+def dictionary
+  substitute =
+    {
+      "hello" => "hi",
+      "to" => '2',
+      "two" => '2',
+      "too" => '2',
+      "for" => '4',
+      "four" => '4',
+      "be" => 'b',
+      "you" => 'u',
+      "at" => "@",
+      "and" => "&"
+    }
+end
+
+
+def word_substituter(tweet)
+#first I turn Str to array to iterate
+  
+  tweet.split()
+
+    shortenedTweet = []
+
+#how iterate over each word
+
+  tweet.each do |word|
+   if dictionary[word.downcase]
+      word = dictionary[word.downcase]
+      shortenedTweet << word
+    else
+      shortenedTweet << word
+    end
+  end
+  shortenedTweet.join(" ")
+end
+
